f67d80626558d3271d76898082d6962a3d15aa2a
gt-ayush <gt-ayush@outlook.com>
2025-11-29 03:02:38 +0530

    soo many updates

--- backend/mesh_project/asgi.py
+++ backend/mesh_project/asgi.py
@@ -1,10 +1,10 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import mesh_app.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mesh_project.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mesh_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
