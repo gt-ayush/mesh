a8fe7c276a0134a1423d289a3ca8838aed9d6d16
gt-ayush <gt-ayush@outlook.com>
2025-11-29 02:28:00 +0530

    update to settings.py

--- backend/mesh_project/settings.py
+++ backend/mesh_project/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,19 +33,24 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mesh_project.urls'
 
+
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / '../frontend/templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ]},
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "../frontend/templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
     },
 ]
+
 
 WSGI_APPLICATION = 'mesh_project.wsgi.application'
 
